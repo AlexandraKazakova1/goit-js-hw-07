04be3e6ec44f27971a405840fba05b90e0d2979d
Alex <zemzikova.oleksandra@gmail.com>
2024-08-19 22:18:43 +0300

    without task-6

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,16 @@
-
+const loginForm = document.querySelector(".login-form");
+loginForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const formElements = event.target.elements;
+    const email = formElements.email.value.trim();
+    const password = formElements.password.value.trim();
+    if (email === '' || password === '') {
+        alert('All form fields must be filled in');
+    }
+    const formData = {
+        email: email,
+        password: password
+    };
+    console.log(formData);
+    event.target.reset();
+});
--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,10 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const changeColor = document.querySelector(".change-color");
+const colorLine = document.querySelector(".color");
+changeColor.addEventListener("click", () => {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  colorLine.textContent = randomColor;
+});
